da717383dc8ee6b41479e66c03aaa1a6d5bce3ea
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2023-06-15 09:50:55 +0700

    Update: Added skeleton loading on home slider

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -51,7 +51,9 @@ const HomeSlider = ({ apiUrl, apiUpcoming, apiSortBy = "popularity.desc" }) => {
         })
         .then((response) => {
           setMovies(response.data.results.slice(0, 5));
-          setLoading(false);
+          setTimeout(() => {
+            setLoading(false);
+          }, 500);
         });
     };
 
@@ -86,7 +88,7 @@ const HomeSlider = ({ apiUrl, apiUpcoming, apiSortBy = "popularity.desc" }) => {
           return (
             <SwiperSlide
               key={index}
-              className="min-h-fit sm:h-[600px] flex items-end relative before:absolute before:inset-0 before:bg-gradient-to-t md:before:bg-gradient-to-r before:from-base-dark-gray after:absolute after:inset-0 after:bg-gradient-to-t lg:after:bg-gradient-to-tr after:from-base-dark-gray lg:after:opacity-[90%]"
+              className="min-h-fit sm:h-[600px] flex items-end relative after:absolute after:inset-0 after:bg-gradient-to-t lg:after:bg-gradient-to-tr after:from-base-dark-gray lg:after:opacity-[90%]"
             >
               <figure className="min-h-fit w-full sm:h-full -z-10 aspect-poster sm:aspect-auto">
                 <img
@@ -102,57 +104,86 @@ const HomeSlider = ({ apiUrl, apiUpcoming, apiSortBy = "popularity.desc" }) => {
                   className="object-top hidden sm:block"
                 />
               </figure>
-              <div className="flex flex-col items-center sm:items-start gap-2 lg:gap-4 z-20 md:max-w-[70%] lg:max-w-[50%] absolute bottom-0 inset-x-0 p-4 xl:p-[4rem]">
+              <div className="flex flex-col items-center sm:items-start gap-2 lg:gap-2 z-20 md:max-w-[70%] lg:max-w-[50%] absolute bottom-0 inset-x-0 p-4 xl:p-[4rem] before:absolute before:inset-0 before:bg-gradient-to-t md:before:bg-gradient-to-r before:from-base-dark-gray h-full justify-end [&_*]:z-10">
                 {/* <h3 className="font-bold text-2xl lg:text-5xl line-clamp-1 lg:line-clamp-2 !leading-tight">
                   {!isTvPage ? movie.title : movie.name}
                 </h3> */}
-                <MovieTitleLogo movie={movie.id} isTvPage={isTvPage} />
-                <div className="flex items-center gap-2 text-sm font-medium">
-                  <div className="flex items-center gap-1">
-                    <IonIcon
-                      icon={star}
-                      className="text-primary-yellow !w-5 h-full aspect-square"
-                    />
-                    <span className="text-base md:text-xl text-white">
-                      {Math.round(movie.vote_average * 10) / 10}
-                    </span>
+
+                {loading ? (
+                  <Loading
+                    height="[150px]"
+                    width="[300px]"
+                    className="w-[300px]"
+                  />
+                ) : (
+                  <MovieTitleLogo movie={movie.id} isTvPage={isTvPage} />
+                )}
+
+                {loading ? (
+                  <Loading
+                    height="[30px]"
+                    width="[150px]"
+                    className="w-[150px]"
+                  />
+                ) : (
+                  <div className="flex items-center gap-2 text-sm font-medium">
+                    <div className="flex items-center gap-1">
+                      <IonIcon
+                        icon={star}
+                        className="text-primary-yellow !w-5 h-full aspect-square"
+                      />
+                      <span className="text-base md:text-xl text-white">
+                        {Math.round(movie.vote_average * 10) / 10}
+                      </span>
+                    </div>
+
+                    <span>&bull;</span>
+                    <div className="whitespace-nowrap flex items-center gap-2">
+                      <span className="text-gray-400">
+                        {new Date(
+                          !isTvPage ? movie.release_date : movie.first_air_date
+                        ).getFullYear()}
+                      </span>
+                    </div>
                   </div>
-                  <span>&bull;</span>
-                  <div className="whitespace-nowrap flex items-center gap-2">
-                    <span className="text-gray-400">
-                      {new Date(
-                        !isTvPage ? movie.release_date : movie.first_air_date
-                      ).getFullYear()}
-                    </span>
+                )}
+
+                {loading ? (
+                  <Loading height="[120px]" className="h-[120px]" />
+                ) : (
+                  <p className="line-clamp-2 md:line-clamp-3">
+                    {movie.overview}
+                  </p>
+                )}
+
+                {!loading && (
+                  <div className="flex gap-2 mt-4 w-full">
+                    <Link
+                      to={!isTvPage ? `/movies/${movie.id}` : `/tv/${movie.id}`}
+                      className="btn bg-primary-blue bg-opacity-60"
+                    >
+                      <IonIcon
+                        icon={informationCircleOutline}
+                        className="!w-5 h-full aspect-square"
+                      />
+                      Details
+                    </Link>
+                    <Link
+                      to={
+                        !isTvPage
+                          ? `/movies/${movie.id}#overview`
+                          : `/tv/${movie.id}#overview`
+                      }
+                      className="btn bg-base-gray bg-opacity-40"
+                    >
+                      <IonIcon
+                        icon={playOutline}
+                        className="!w-5 h-full aspect-square"
+                      />
+                      Trailer
+                    </Link>
                   </div>
-                </div>
-                <p className="line-clamp-2 md:line-clamp-3">{movie.overview}</p>
-                <div className="flex gap-2 mt-4 w-full">
-                  <Link
-                    to={!isTvPage ? `/movies/${movie.id}` : `/tv/${movie.id}`}
-                    className="btn bg-primary-blue bg-opacity-60"
-                  >
-                    <IonIcon
-                      icon={informationCircleOutline}
-                      className="!w-5 h-full aspect-square"
-                    />
-                    Details
-                  </Link>
-                  <Link
-                    to={
-                      !isTvPage
-                        ? `/movies/${movie.id}#overview`
-                        : `/tv/${movie.id}#overview`
-                    }
-                    className="btn bg-base-gray bg-opacity-40"
-                  >
-                    <IonIcon
-                      icon={playOutline}
-                      className="!w-5 h-full aspect-square"
-                    />
-                    Trailer
-                  </Link>
-                </div>
+                )}
               </div>
             </SwiperSlide>
           );
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
-export function Loading({ height = "full" }) {
+export function Loading({ height = "full", width = "full" }) {
   return (
-    <div className={`h-${height} w-full`}>
+    <div className={`h-${height} w-${width}`}>
       <div
         className={`animate-pulse h-full bg-base-gray bg-opacity-50 rounded`}
       ></div>
