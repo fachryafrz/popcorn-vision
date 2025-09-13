d86ab3c43e62d289523ce9bd4c0afc4f32315286
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-09-14 01:21:47 +0700

    fix: forgot to remove next-client-cookies package

--- src/components/Providers/index.jsx
+++ src/components/Providers/index.jsx
@@ -1,10 +1,5 @@
-import { CookiesProvider } from "next-client-cookies/server";
 import ProgressProvider from "../Layout/ProgressBarProvider";
 
 export default function Providers({ children }) {
-  return (
-    <CookiesProvider>
-      <ProgressProvider>{children}</ProgressProvider>
-    </CookiesProvider>
-  );
+  return <ProgressProvider>{children}</ProgressProvider>;
 }
