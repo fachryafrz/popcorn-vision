275797893fb5e2bafa685665dbd7a8323ddf2b3e
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2026-08-08 18:59:12 +0700

    fix: build errors

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 import { ConvexReactClient, ConvexProvider } from "convex/react";
-import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { ConvexBetterAuthProvider, type AuthClient } from "@convex-dev/better-auth/react";
 import { authClient } from "@/lib/auth-client";
 import { TooltipProvider } from "./ui/tooltip";
 import { PersonalizationProvider } from "./theme-provider";
@@ -14,7 +14,7 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ConvexProvider client={convex}>
-      <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      <ConvexBetterAuthProvider client={convex} authClient={authClient as unknown as AuthClient}>
         <PersonalizationProvider>
           <ConfirmProvider>
             <TooltipProvider>
